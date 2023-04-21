11e2249142a13e0d6e5f32fbac983ab6fd957ccc
Wedney <devwedney@outlook.com>
2023-04-21 18:30:28 -0300

    node.fs.4

--- modules/fs.js
+++ modules/fs.js
@@ -1,11 +1,43 @@
 const fs = require("fs");
 const path = require("path");
 
-// Cira uma pasta
-fs.mkdir(path.join(__dirname, "/test"), (error) => {
-    if(error) {
-        return console.log("Erro", error);
-    }
+// Cria uma pasta
+// fs.mkdir(path.join(__dirname, "/test"), (error) => {
+//     if(error) {
+//         return console.log("Erro", error);
+//     }
 
-    console.log("Pasta criada com sucesso!");
-})
+//     console.log("Pasta criada com sucesso!");
+// });
+
+// Cria um arquivo
+fs.writeFile(
+    path.join(__dirname, "/test", "test.txt"), "Hello node!",(error) => {
+        if (error) {
+            return console.log("Erro: ", error);
+        }
+        console.log("Arquivo Criado com Sucesso");
+    }
+);
+
+// Adicionar á um arquivo
+fs.appendFile(
+    path.join(__dirname, "/test", "test.txt"), " Hello Word!", (error) => {
+        if (error) {
+            return console.log("Erro: ", error);
+        }
+
+        console.log("Arquivo modificicado com suecsso!");
+    }
+);
+
+// Ler arquivo
+fs.readFile(
+    path.join(__dirname, "/test", "test.txt"), "utf8", (error, data) => {
+        if (error) {
+            return console.log("Erro: ", error);
+        }
+
+        console.log(data);
+    }
+);
